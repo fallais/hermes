1226c799e712d6988c387d78cea86108f6beeafd
François Allais <francois.allais@hotmail.com>
2019-03-08 17:39:04 +0100

    remove protocols

--- protocols/contact.go
+++
@@ -1,8 +0,0 @@
-package protocols
-
-// Contact is a contact.
-type Contact struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-	Birthdate string `json:"birthdate"`
-}
